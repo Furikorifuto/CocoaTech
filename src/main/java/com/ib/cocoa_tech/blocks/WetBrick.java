cb2f287b051730a9c1245e0b25242fc2552d86d8
kyuta683 <k.yuta.rlc@outlook.com>
2025-05-15 14:16:26 +0900

    0515PM

--- src/main/java/com/ib/cocoa_tech/blocks/WetBrick.java
+++ src/main/java/com/ib/cocoa_tech/blocks/WetBrick.java
@@ -1,26 +1,64 @@
 package com.ib.cocoa_tech.blocks;
 
+import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
-public class WetBrick extends Block {
-    protected static final VoxelShape SHAPE = Block.box(4.0, 0.0, 4.0, 12.0, 12.0, 12.0);
-    public WetBrick(Properties p_49795_) {
-        super(p_49795_
-                .dynamicShape()
+public class WetBrick extends HorizontalDirectionalBlock {
+    public static final MapCodec<WetBrick> CODEC = simpleCodec(WetBrick::new);
+    protected static final VoxelShape SHAPE_H = Block.box(1.0, 0.0, 4.0, 15.0, 4.0, 12.0);
+    protected static final VoxelShape SHAPE_V = Block.box(4.0, 0.0, 1.0, 12.0, 4.0, 15.0);
+    public WetBrick(Properties properties) {
+        super(properties
                 .instabreak()
+                .randomTicks()
                 .sound(SoundType.FROGLIGHT));
     }
     @Override
-    protected @NotNull VoxelShape getShape(BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
-        Vec3 vec3 = state.getOffset(pos);
-        return SHAPE.move(vec3.x, vec3.y, vec3.z);
+    protected @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
+        return switch(state.getValue(FACING)){
+            case NORTH, SOUTH -> SHAPE_V;
+            default -> SHAPE_H;
+        };
+    }
+    @Override
+    protected @NotNull MapCodec<? extends HorizontalDirectionalBlock> codec() {
+        return CODEC;
+    }
+
+    @Override
+    protected void tick(@NotNull BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
+        if(!canSurvive(state,level,pos)){
+            level.destroyBlock(pos,true);
+        }
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    protected boolean canSurvive(@NotNull BlockState state, @NotNull LevelReader level, @NotNull BlockPos pos) {
+        BlockState blockState = level.getBlockState(pos.below());
+        return blockState.isSolidRender();
     }
 }
